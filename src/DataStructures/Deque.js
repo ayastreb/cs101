60607af41841cb1f1c2e97a164f757f2c7c7a077
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-09-17 12:57:43 +0200

    Implement deque

---
+++ src/DataStructures/Deque.js
@@ -0,0 +1,133 @@
+const createDoublyLinkedList = require('./DoublyLinkedList')
+/**
+ * Deque (double-ended queue, pronounced "deck") is a generalization of a queue
+ * where elements can be added to or removed from either the front or the back.
+ *
+ * Application:
+ * One example where a deque can be used is the A-Steal job scheduling algorithm.
+ * This algorithm implements task scheduling for several processors.
+ * A separate deque with threads to be executed is maintained for each processor.
+ * To execute the next thread, the processor gets the first element from the deque
+ * (using the "remove first element" deque operation). If the current thread forks,
+ * it is put back to the front of the deque ("insert element at front")
+ * and a new thread is executed. When one of the processors finishes execution of
+ * its own threads (i.e. its deque is empty), it can "steal" a thread from
+ * another processor: it gets the last element from the deque of another
+ * processor ("remove last element") and executes it.
+ * The steal-job scheduling algorithm is used by
+ * Intel's Threading Building Blocks (TBB) library for parallel programming.
+ *
+ * Usage:
+ * const createDeque = require('./Deque')
+ * const deque = createDeque([ 'A', 'B', 'C' ]) // deque = A->B->C
+ * deque.first()      // => 'A', deque = A->B->C
+ * deque.last()       // => 'C', deque = A->B->C
+ * deque.push('D')    // deque = A->B->C->D
+ * deque.shift()      // => 'A', deque = B->C->D
+ * deque.pop()        // => 'D', deque = B->C
+ * deque.size()       // => 2, deque = B->C
+ * deque.unshift('A') // deque = A->B->C
+ * deque.pop()        // => 'C', deque = A->B
+ * deque.pop()        // => 'B', deque = A
+ * deque.pop()        // => 'A', deque = null
+ * deque.pop()        // throws RangeError when trying to remove from empty queue
+ *
+ * @param {Array} input initial deque data
+ */
+module.exports = (input = []) => {
+  const list = createDoublyLinkedList()
+
+  initialize(input)
+  /** Public interface */
+  return {
+    size,
+    first,
+    last,
+    push,
+    unshift,
+    pop,
+    shift
+  }
+
+  /**
+   * Add all elements of given array to the deque.
+   *
+   * Performance: O(n)
+   * @param {Array} input initial deque data
+   */
+  function initialize (input) {
+    input.forEach(push)
+  }
+
+  /**
+   * Get current deque size.
+   *
+   * Performance: O(1)
+   * @returns {number}
+   */
+  function size () {
+    return list.size()
+  }
+
+  /**
+   * Return value of first element without removing it.
+   *
+   * Performance: O(1)
+   * @returns {*}
+   */
+  function first () {
+    return list.size() > 0 ? list.headNode().data : null
+  }
+
+  /**
+   * Return value of last element without removing it.
+   *
+   * Performance: O(1)
+   * @returns {*}
+   */
+  function last () {
+    return list.size() > 0 ? list.tailNode().data : null
+  }
+
+  /**
+   * Add new element to the back of the deque.
+   *
+   * Performance: O(1)
+   * @param {*} item
+   */
+  function push (item) {
+    list.addLast(item)
+  }
+
+  /**
+   * Add new element to the front of the deque.
+   *
+   * Performance: O(1)
+   * @param {*} item
+   */
+  function unshift (item) {
+    list.addFirst(item)
+  }
+
+  /**
+   * Remove and return element from the back of the deque.
+   *
+   * Performance: O(1)
+   * @returns {*}
+   */
+  function pop () {
+    if (list.size() === 0) throw RangeError("Can't pop empty queue.")
+    return list.removeLast()
+  }
+
+  /**
+   * Remove and return element from the front of the deque.
+   *
+   * Performance: O(1)
+   * @returns {*}
+   */
+  function shift () {
+    if (list.size() === 0) throw RangeError("Can't shift empty queue.")
+    return list.removeFirst()
+  }
+}
---
+++ test/DataStructures/DequeSpec.js
@@ -0,0 +1,57 @@
+const test = require('tape')
+const createDeque = require('../../src/DataStructures/Deque')
+
+test('create empty deque', assert => {
+  const deque = createDeque()
+  assert.equal(deque.size(), 0)
+  assert.strictEqual(deque.first(), null)
+  assert.strictEqual(deque.last(), null)
+  assert.end()
+})
+
+test('create deque from initial data', assert => {
+  const deque = createDeque(['A', 'B', 'C'])
+  assert.equal(deque.size(), 3)
+  assert.equal(deque.first(), 'A')
+  assert.equal(deque.last(), 'C')
+  assert.end()
+})
+
+test('add item to the front of the deque', assert => {
+  const deque = createDeque()
+  deque.push('A')
+  deque.push('B')
+  assert.equal(deque.first(), 'A')
+  assert.equal(deque.last(), 'B')
+  assert.end()
+})
+
+test('add item to the back of the deque', assert => {
+  const deque = createDeque(['B', 'C', 'D'])
+  deque.unshift('A')
+  assert.equal(deque.first(), 'A')
+  assert.end()
+})
+
+test('remove item from the front of the deque', assert => {
+  const deque = createDeque(['A', 'B', 'C'])
+  assert.equal(deque.pop(), 'C')
+  assert.equal(deque.first(), 'A')
+  assert.equal(deque.last(), 'B')
+  assert.end()
+})
+
+test('remove item from the back of the deque', assert => {
+  const deque = createDeque(['A', 'B', 'C'])
+  assert.equal(deque.shift(), 'A')
+  assert.equal(deque.first(), 'B')
+  assert.equal(deque.last(), 'C')
+  assert.end()
+})
+
+test('throws error when trying to remove from empty deque', assert => {
+  const deque = createDeque()
+  assert.throws(() => deque.pop(), RangeError)
+  assert.throws(() => deque.shift(), RangeError)
+  assert.end()
+})
