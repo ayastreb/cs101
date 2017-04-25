db28f6b3a805e1c5ade7fa55135df856f0f15d8d
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-04-25 10:47:49 +0900

    Add level order traversal.

---
+++ src/Problems/LevelOrderTraversal.js
@@ -0,0 +1,29 @@
+const createQueue = require('../DataStructures/Queue')
+/**
+ * Given a binary tree, traverse it in level order, e.g. print each level of
+ * the tree in a separate line.
+ *
+ * @param {BinarySearchTree} root
+ * @returns {Array}
+ */
+module.exports = root => {
+  let pendingNodes = 0
+  const output = []
+  const queue = createQueue()
+  queue.enqueue(root)
+
+  while (queue.size() > 0) {
+    const level = []
+    pendingNodes = queue.size()
+    while (pendingNodes > 0) {
+      const node = queue.dequeue()
+      level.push(node.value)
+      if (node.left) queue.enqueue(node.left)
+      if (node.right) queue.enqueue(node.right)
+      pendingNodes--
+    }
+    output.push(level)
+  }
+
+  return output
+}
