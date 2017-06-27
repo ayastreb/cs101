3e3b2584755ecf1a65474d5080a3c5cb69fd1869
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-06-27 16:23:11 +0900

    Add key-value linked list

---
+++ src/DataStructures/KeyValueLinkedList.js
@@ -0,0 +1,65 @@
+/**
+ * Doubly linked list which stores key/value pairs and exposes internal nodes,
+ * e.g. `find` returns a node and `remove` expects a node as an argument.
+ */
+module.exports = () => {
+  let head
+  let length = 0
+
+  return {
+    get length() {
+      return length
+    },
+    insert: (key, value) => {
+      const node = {
+        next: head,
+        prev: undefined,
+        key,
+        value
+      }
+      if (head) head.prev = node
+      head = node
+      length++
+    },
+    find: key => {
+      let current = head
+      while (current && current.key !== key) {
+        current = current.next
+      }
+
+      return current
+    },
+    remove: node => {
+      if (!node) return false
+      if (!node.prev) {
+        head = node.next
+        if (head) head.prev = undefined
+      } else {
+        node.prev.next = node.next
+        if (node.next) node.next.prev = node.prev
+      }
+      length--
+      return true
+    },
+    keys: () => {
+      const keys = []
+      let current = head
+      while (current) {
+        keys.push(current.key)
+        current = current.next
+      }
+
+      return keys
+    },
+    values: () => {
+      const values = []
+      let current = head
+      while (current) {
+        values.push(current.value)
+        current = current.next
+      }
+
+      return values
+    }
+  }
+}
