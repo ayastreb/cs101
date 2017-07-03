c66bc0d113864f9932bc68216aafe5650068cbb9
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-07-03 11:58:45 +0900

    Use loop instead of linked list.

--- src/Problems/CountUniqueSorted.js
+++ src/Problems/CountUniqueSorted.js
@@ -1,22 +1,20 @@
-const createLinkedList = require('../DataStructures/SinglyLinkedList')
 /**
  * Given a sorted array of numbers, return the number of unique numbers.
  * Example:
  * [1, 3, 3, 5, 6, 7, 7, 7, 15] => 6 (1, 3, 5, 6, 7, 15)
  *
- * We can solve it using linked list, since all the items are sorted.
- * We can iterate input array and add number to the list
- * only if it's different from list head.
- * Size of the list will be equal to number of unique numbers in the end.
+ * We can iterate over array and compare each item with next item.
  *
  * Performance: O(n)
  * @param {Array} input
  */
 module.exports = input => {
-  const list = createLinkedList()
-  for (let item of input) {
-    if (item !== list.first) list.addFirst(item)
+  if (input.length <= 1) return input.length
+
+  let counter = 1
+  for (let i = 0; i < input.length - 1; i++) {
+    if (input[i] !== input[i + 1]) counter++
   }
 
-  return list.length
+  return counter
 }
--- src/Problems/CountUniqueSorted.test.js
+++ src/Problems/CountUniqueSorted.test.js
@@ -1,13 +1,24 @@
 const test = require('tape')
 const countUnique = require('./CountUniqueSorted')
 
+test('it counts zero items', assert => {
+  assert.equal(countUnique([]), 0)
+  assert.end()
+})
+
 test('it counts single item', assert => {
   assert.equal(countUnique([5]), 1)
   assert.end()
 })
 
+test('it counts single unique item', assert => {
+  assert.equal(countUnique([5, 5, 5]), 1)
+  assert.end()
+})
+
 test('it counts multiple items', assert => {
   assert.equal(countUnique([1, 3, 3, 5]), 3)
+  assert.equal(countUnique([1, 3, 3, 4, 5, 5, 5]), 4)
   assert.end()
 })
 
