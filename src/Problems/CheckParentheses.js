7a652fd22dccc8e3de1a5bde582589c59d2e2d05
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-08-24 10:38:45 +0900

    Add parentheses problem

---
+++ src/Problems/CheckParentheses.js
@@ -0,0 +1,26 @@
+const createQueue = require('../DataStructures/Queue')
+/**
+ * Given an input string we need to check if all parentheses are balanced (closed).
+ *
+ * Performance: O(n)
+ * @param {string} input
+ * @return {boolean|number} true if parentheses are balanced, position of first
+ * offender otherwise.
+ */
+module.exports = input => {
+  const OPEN = '('
+  const CLOSED = ')'
+  const queue = createQueue()
+
+  const parentheses = input.split('')
+  for (let position = 0; position < parentheses.length; position++) {
+    if (parentheses[position] === OPEN) {
+      queue.enqueue(position)
+    } else if(parentheses[position] === CLOSED) {
+      if (queue.length === 0) return position
+      queue.dequeue()
+    }
+  }
+
+  return queue.length === 0 ? true : queue.dequeue()
+}
