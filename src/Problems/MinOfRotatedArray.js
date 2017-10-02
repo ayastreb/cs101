b05f7b8acf3e6aa616233cc4179c9bd2df3c8611
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-10-02 13:49:18 +0900

    Add min of rotated array

---
+++ src/Problems/MinOfRotatedArray.js
@@ -0,0 +1,17 @@
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * Find the minimum element.
+ *
+ * @param {array} nums
+ */
+module.exports = nums => {
+  function search(lo, hi) {
+    if (lo >= hi || nums[lo] < nums[hi]) return nums[lo]
+
+    const mid = Math.floor((lo + hi) / 2)
+    return nums[mid] >= nums[lo] ? search(mid + 1, hi) : search(lo, mid)
+  }
+
+  return search(0, nums.length - 1)
+}
