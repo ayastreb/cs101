0cd93621734ff88246dc507c06aa7e57b80169bb
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2018-01-13 19:29:04 +0900

    Refactor bubble sort

--- src/Algorithms/Sort/BubbleSort.js
+++ src/Algorithms/Sort/BubbleSort.js
@@ -1,12 +1,17 @@
 module.exports = input => {
-  for (let outer = input.length; outer > 0; outer--) {
-    for (let inner = 0; inner < outer; inner++) {
+  let isSorted = false
+  let unsortedLength = input.length
+  while (!isSorted) {
+    isSorted = true
+    for (let inner = 0; inner < unsortedLength; inner++) {
       if (input[inner] > input[inner + 1]) {
         let tmp = input[inner + 1]
         input[inner + 1] = input[inner]
         input[inner] = tmp
+        isSorted = false
       }
     }
+    unsortedLength--
   }
 
   return input
