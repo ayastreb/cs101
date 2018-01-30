155a14ea391d77f810a647796f406bc25e7af0c9
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2018-01-30 16:47:13 +0900

    Add regex match

---
+++ src/Problems/BasicRegex.js
@@ -0,0 +1,27 @@
+/**
+ * Check if given pattern matches the text with suppor for '.' and '*' symbols.
+ * '.' is treated as a single a character wildcard,
+ * and '*' is matched for a zero or more sequence of the previous letter.
+ *
+ * @param {String} text
+ * @param {String} pattern
+ * @returns {Boolean}
+ */
+module.exports = (text, pattern) => {
+  let tIndex = 0
+  let pIndex = 0
+  while (tIndex < text.length || pIndex < pattern.length) {
+    if (pattern[pIndex + 1] === '*') {
+      const lookFor = pattern[pIndex] === '.' ? text[tIndex] : pattern[pIndex]
+      while (text[tIndex] === lookFor) tIndex++
+      pIndex += 2
+    } else if (pattern[pIndex] === '.' || pattern[pIndex] === text[tIndex]) {
+      tIndex++
+      pIndex++
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
